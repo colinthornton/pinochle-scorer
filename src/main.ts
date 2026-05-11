87c51659ed3c10e3d40f045ee27667944578f2aa
Colin Thornton <colinthorntontx@gmail.com>
2026-05-11 11:59:46 +0900

    Set up Prime Vue UI

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { primeVuePlugin } from "./plugins/prime_vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(primeVuePlugin);
+
+app.mount("#app");
